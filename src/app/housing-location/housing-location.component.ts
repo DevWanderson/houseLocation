b3a3f3e660e2e5b8bd414faf6997c2acb12e908c
Wanderson Guedes Lopes <wanderson.lopes@iti.gov.br>
2025-05-23 15:48:44 -0300

    Upload do projeto: Locação de casas

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styles: ``
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: Housinglocation;
+}
